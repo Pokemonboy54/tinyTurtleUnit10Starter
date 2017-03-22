3e1adcc8789310cf07c6a7ee2387c3220ded1c2b
Sammy Ech-Chaouy <sech-chaouy@aclanyc.com>
2017-03-22 12:35:38 +0000

    hi

--- script.js
+++ script.js
@@ -12,7 +12,59 @@ right(90);
 forward(50);
 }
 
+function triangle(y){
+right(30);
+forward(y);
+right(120);
+forward(y);
+right(120);
+forward(y);
+right(120);
+left(30);
+}
+function rectangle(x){
+forward(x);
+right(90);
+forward(x);
+right(90);
+forward(x);
+right(90);
+forward(x);
+right(90);
+forward(x);
+}
+
+function house(){
+forward(40);
+right(35);
+forward(20);
+right(105);
+forward(20);
+right(125);
+forward(25);
+left(180);
+forward(25);
+right(90);
+forward(40);
+right(90);
+forward(25);
+}
+
+function pentagon(size){
+forward(size);
+right(72);
+forward(size);
+right(72);
+forward(size);
+right(72);
+forward(size);
+right(72);
+forward(size);
+right(72);
+}
+
 // Type your function call below
-
-
+rectangle(100);
+triangle(100);
+pentagon(100)
 stamp();
